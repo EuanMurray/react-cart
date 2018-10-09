b1b2f4cdf22d2a63b54dec743fc94a59116e60e0
Euan Murray <euan.murray@itftennis.com>
2018-10-09 16:38:37 +0100

    no message

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
-class Cart extends Component {
+class Counter extends Component {
   state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
+    // tags: ["tag1", "tag2", "tag3"]
   };
 
   //   constructor() {
@@ -12,13 +12,14 @@ class Cart extends Component {
   //   }
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     const product = { id: 1 };
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement(product)}
@@ -26,20 +27,21 @@ class Cart extends Component {
         >
           Increment
         </button>
+        <button className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
-export default Cart;
+export default Counter;
