9d49386c22effbab6e7b2f5d72fc2bb1bb028a92
Euan Murray <euan.murray@itftennis.com>
2018-10-09 14:40:25 +0100

    Initial commit

---
+++ src/components/cart.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Cart extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    const product = { id: 1 };
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Cart;
